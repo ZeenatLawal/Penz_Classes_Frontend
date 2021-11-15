594ed6fef7a35368aa1ca5ea31f868662d600264
elyor doniyorov <doniyorovelyor@gmail.com>
2021-11-15 20:10:31 +0500

    Add Router to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-    </div>
+    </Router>
   );
 }
 
