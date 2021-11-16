3a33491580b1bbd94ec0a94f94972f51c7e4c2ac
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-16 21:44:30 +0100

    Send reservation data to Api

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,26 @@
+const url = 'http://localhost:3000/api/v1/courses';
+
 const getCourses = async () => {
-  const request = await fetch('http://localhost:3000/api/v1/courses');
+  const request = await fetch(url);
   const result = await request.json();
   return result;
 };
 
-export default getCourses;
+const reserveCourse = async (course) => {
+  const result = await fetch('http://localhost:3000/api/v1/reservations', {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      user_id: course.user,
+      course_id: course.courseId,
+      start_date: course.date,
+    }),
+  });
+
+  return result.status === 201;
+};
+
+export { getCourses, reserveCourse };
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import coursesReducer from './courses/Courses';
+import reservationsReducer from './reservations/Reservations';
 
 const reducer = combineReducers({
   coursesReducer,
+  reservationsReducer,
 });
 
 const store = createStore(
