b0a1184dde232d7090a2111f84e450217a56dc51
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-16 10:22:26 +0100

    Create log in and signup page

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => (
+  <form>
+    <h2>Log In</h2>
+    <input type="text" placeholder="Please enter your username" />
+    <button type="button">Log In</button>
+  </form>
+);
+
+export default Login;
---
+++ src/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => (
+  <form>
+    <h2>Sign Up</h2>
+    <input type="text" placeholder="Please enter your username" />
+    <button type="button">Sign Up</button>
+  </form>
+);
+
+export default SignUp;
