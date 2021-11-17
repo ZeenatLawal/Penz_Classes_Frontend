2e2f536096e8b5f5a16032389d7a6b9e1cb24de6
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-17 17:58:29 +0100

    Fetch and display the list of all courses with title and instructor name

--- src/pages/DeleteCourse.js
+++ src/pages/DeleteCourse.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { loadCourses } from '../redux/courses/Courses';
 
-const DeleteCourse = () => (
-  <div>Hello</div>
-);
+const DeleteCourse = () => {
+  const courses = useSelector((state) => state.coursesReducer.courses);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadCourses());
+  }, [dispatch]);
+
+  return (
+    <div>
+      {courses && courses.map((course) => (
+        <p value={course.id} key={course.id}>
+          {' '}
+          {course.title}
+          {' '}
+          by
+          {' '}
+          {course.instructor}
+          {' '}
+        </p>
+      ))}
+    </div>
+  );
+};
 
 export default DeleteCourse;
