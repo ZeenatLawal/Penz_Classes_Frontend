16d2026722d98f76bce967b3778002ed1c01e5e8
enkog <nkirukaogu@gmail.com>
2021-11-17 15:27:40 +0100

    Dispatch an action to signup a user

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import RegisterAuthAction from '../redux/users/actions/RegisterAuthAction';
 
-const SignUp = () => (
-  <form>
-    <h2>Sign Up</h2>
-    <input type="text" placeholder="Please enter your username" />
-    <button type="button">Sign Up</button>
-  </form>
-);
+const SignUp = () => {
+  const [userState, setUserState] = useState({});
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(RegisterAuthAction(userState));
+  };
+
+  return (
+    <form onSubmit={(e) => handleSubmit(e)}>
+      <h2>Sign Up</h2>
+      <input
+        type="text"
+        placeholder="Please enter your username"
+        onChange={(event) => {
+          const username = event.target.value;
+          setUserState({ ...userState, ...{ username } });
+        }}
+      />
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
 
 export default SignUp;
--- src/redux/users/reducers/rootReducer.js
+++ src/redux/users/reducers/rootReducer.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import authReducer from './AuthReducer';
 
 const rootReducer = combineReducers({
-  authReducer,
+  auth: authReducer,
 });
 
 export default rootReducer;
