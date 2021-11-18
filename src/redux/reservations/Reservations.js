e9df2188d260fd8db58158aad9eeeefa075199df
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-18 08:52:17 +0100

    Fetch reservations and add action reducer

--- src/redux/reservations/Reservations.js
+++ src/redux/reservations/Reservations.js
@@ -1,8 +1,35 @@
 import { reserveCourse } from '../../Api';
 
 const RESERVE_COURSE = 'penz_classes_frontend/courses/RESERVE_COURSE';
+const FETCH_COURSES_STARTED = 'penz_classes_frontend/courses/FETCH_COURSE_STARTED';
+const FETCH_COURSES_SUCCEED = 'penz_classes_frontend/courses/FETCH_COURSE_SUCCEED';
+const FETCH_COURSES_FAILED = 'penz_classes_frontend/courses/FETCH_COURSE_FAILED';
 
-const initialState = [];
+const initialState = {
+  status: 'awaiting',
+  message: '',
+};
+
+const fetchCoursesStarted = () => ({
+  type: FETCH_COURSES_STARTED,
+});
+export const fetchCoursesSucceed = (payload) => ({
+  type: FETCH_COURSES_SUCCEED,
+  payload,
+});
+const fetchCoursesFailed = (payload) => ({
+  type: FETCH_COURSES_FAILED,
+  payload,
+});
+
+export const fetchReserveCourses = () => async (dispatch) => {
+  const url = 'http://localhost:3000/v1/reservations';
+  dispatch(fetchCoursesStarted());
+  await fetch(url)
+    .then((response) => response.json())
+    .then((json) => dispatch(fetchCoursesSucceed(json)))
+    .catch((error) => dispatch(fetchCoursesFailed(error.toString())));
+};
 
 export const reserve = (course) => async (dispatch) => {
   const created = await reserveCourse(course);
@@ -17,6 +44,22 @@ export const reserve = (course) => async (dispatch) => {
 
 const reservationsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_COURSES_STARTED:
+      return {
+        ...state,
+        status: 'loading',
+      };
+    case FETCH_COURSES_SUCCEED:
+      return {
+        message: action.payload.messsage,
+        status: 'awaiting',
+      };
+    case FETCH_COURSES_FAILED:
+      return {
+        ...state,
+        status: 'failed',
+        error: action.payload,
+      };
     case RESERVE_COURSE:
       return {
         ...state,
