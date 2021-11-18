37ed2038159f639b250d21638cab2ff880e87358
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-18 20:30:48 +0100

    Style navlinks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import ReserveForm from './pages/ReserveForm';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom';
 import NavbarFooter from './NavbarFooter';
 
 const Navbar = () => (
-  <div className="w-25 p-2 border-end">
-    <h2>PENZ-Classes</h2>
+  <div className="w-25 p-2 border-end d-flex flex-column justify-content-between align-items-center nav">
     <nav>
+      <h3 className="pb-5 pt-3 title">PENZ-Classes</h3>
       <ul>
-        <li><NavLink to="/courses">Courses</NavLink></li>
-        <li><NavLink to="/reserve">Reserve Form</NavLink></li>
-        <li><NavLink to="/reservations">My Reservations</NavLink></li>
-        <li><NavLink to="/create-course">Create a Course</NavLink></li>
-        <li><NavLink to="/delete-courses">Delete Courses</NavLink></li>
+        <li><NavLink activeClassName="active" to="/courses">Courses</NavLink></li>
+        <li><NavLink activeClassName="active" to="/reserve">Reserve Form</NavLink></li>
+        <li><NavLink activeClassName="active" to="/reservations">My Reservations</NavLink></li>
+        <li><NavLink activeClassName="active" to="/create-course">Create a Course</NavLink></li>
+        <li><NavLink activeClassName="active" to="/delete-courses">Delete Courses</NavLink></li>
       </ul>
     </nav>
     <NavbarFooter />
