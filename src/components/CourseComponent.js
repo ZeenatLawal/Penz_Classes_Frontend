47c1783df70ef3568c75c2d09c2934b6fa2ed309
elyor doniyorov <doniyorovelyor@gmail.com>
2021-11-18 21:29:48 +0500

    Display all courses' title, description, instructor, img of the course

--- src/components/CourseComponent.js
+++ src/components/CourseComponent.js
@@ -1,25 +1,28 @@
 /* eslint-disable */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 
 const CourseComponent = ({course}) => {
   const dispatch = useDispatch();
   return (
     <div key={course.id}>
-      {course.title}
-      {course.instructor},
-      {course.description},
-      {/* {course.image.image} */}
+      <h2>{course.title}</h2>
+      <h3>{course.instructor}</h3>
+      <p>{course.description}</p>
+      <img src={course.image}/>
     </div>
   );
 };
 
-// CourseComponent.propTypes = {
-//     course: PropTypes.shape({
-//       id: PropTypes.number,
-//       title: PropTypes.string,
-//       instructor: PropTypes.string,
-//     }).isRequired,
-//   };
+CourseComponent.propTypes = {
+    course: PropTypes.shape({
+      id: PropTypes.number,
+      title: PropTypes.string,
+      instructor: PropTypes.string,
+      description: PropTypes.string,
+      image: PropTypes.string,
+    }).isRequired,
+};
 
 export default CourseComponent;
