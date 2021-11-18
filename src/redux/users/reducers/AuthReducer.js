d8990b61f2bf3127c7c63da52622408182d15436
enkog <nkirukaogu@gmail.com>
2021-11-18 12:33:43 +0100

    Createreducer for user logout

--- src/redux/users/reducers/AuthReducer.js
+++ src/redux/users/reducers/AuthReducer.js
@@ -1,16 +1,26 @@
 import { ActionType } from '../actions/AuthAction';
 
-const authReducer = (state = {}, action) => {
+const authState = {
+  isLoggedIn: false,
+  user: {
+    username: '',
+    token: '',
+  },
+};
+const getAuthState = () => authState;
+const newAuth = getAuthState();
+
+const authReducer = (state = newAuth, action) => {
   switch (action.type) {
     case ActionType.REGISTER_SUCCESS:
       return {
-        isSignedUp: true,
+        isLoggedIn: true,
         user: action.payload,
         message: 'User was created successfully',
       };
     case ActionType.REGISTER_FAIL:
       return {
-        isSignedUp: false,
+        ...authState,
         message: action.payload,
       };
     case ActionType.LOGIN_SUCCESS:
@@ -21,6 +31,12 @@ const authReducer = (state = {}, action) => {
     case ActionType.LOGIN_FAIL:
       return {
         isLoggedIn: false,
+        message: action.payload,
+      };
+    case ActionType.LOGOUT_SUCCESS:
+      return authState;
+    case ActionType.LOGOUT_FAIL:
+      return {
         message: action.payload,
       };
     default:
