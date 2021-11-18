36829305675445ac282470729e0184c540cca72a
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-11-18 08:16:26 +0100

    Implent delete function on button click and display new list of courses

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
+import { removeCourse } from '../redux/courses/Courses';
 
-const DeleteButton = ({ course }) => (
-  <tr key={course.id}>
-    <td className="col-5">{course.title}</td>
-    <td className="col-5">{course.instructor}</td>
-    <td>
-      <Button variant="outline-danger" size="sm">Delete Course</Button>
-    </td>
-  </tr>
-);
+const DeleteButton = ({ course }) => {
+  const dispatch = useDispatch();
+
+  const remove = (id) => {
+    dispatch(removeCourse(id));
+  };
+
+  return (
+    <tr key={course.id}>
+      <td className="col-5">{course.title}</td>
+      <td className="col-5">{course.instructor}</td>
+      <td>
+        <Button variant="outline-danger" size="sm" onClick={() => remove(course.id)}>Delete Course</Button>
+      </td>
+    </tr>
+  );
+};
 
 DeleteButton.propTypes = {
   course: PropTypes.shape({
