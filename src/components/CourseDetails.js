8c663094133542e9da33189c8307664d7803feb8
enkog <nkirukaogu@gmail.com>
2021-11-19 22:41:35 +0100

    Reserve selected course

--- src/components/CourseDetails.js
+++ src/components/CourseDetails.js
@@ -3,18 +3,30 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectedCourse } from '../redux/courses/Courses';
+import { reserve } from '../redux/reservations/Reservations';
 
 const CourseDetails = () => {
-  const courses = useSelector((state) => state.coursesReducer);
   const dispatch = useDispatch();
   const { courseId } = useParams();
+  const courses = useSelector((state) => state.coursesReducer);
+  const user = useSelector((state) => state.authReducer.user.user_id);
+
   const {
-    id, title, instructor_name: instructorName, description, image,
+    id, title, instructor_name: instructorName, image,
   } = courses;
 
   const fetchSelectedCourse = async () => {
     const res = await axios.get(`http://localhost:3000/api/v1/courses/${courseId}`);
     dispatch(selectedCourse(res.data));
+  };
+
+  const handleReservation = () => {
+    const reservationInfo = {
+      user,
+      courseId: courses.id,
+      date: '11-2-2021',
+    };
+    dispatch(reserve(reservationInfo));
   };
 
   useEffect(() => {
@@ -24,8 +36,13 @@ const CourseDetails = () => {
   }, [courseId]);
 
   return (
-    <div>
-      <h1>{courseId}</h1>
+    <div className="container">
+      <div key={id}>
+        <div><img src={image} alt="" /></div>
+        <div>{title}</div>
+        <div>{instructorName}</div>
+      </div>
+      <button className="btn btn-secondary" type="button" onClick={handleReservation}>Reserve</button>
     </div>
   );
 };
