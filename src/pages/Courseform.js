f0070658944e3ea7aaacc82e1c736eea9bae175f
prolajumokeoni <prolajumokeoni@gmail.com>
2021-11-22 11:42:06 +0100

    Add validations and redirect on form submit

--- src/pages/Courseform.js
+++ src/pages/Courseform.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 import { addNewCourses } from '../redux/courses/Courses';
 
 const Courseform = () => {
@@ -10,6 +11,12 @@ const Courseform = () => {
   const [description, setDescription] = useState('');
   const [image, setImage] = useState('');
   const [instructor, setInstructor] = useState('');
+  const [validated, setValidated] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRoutes = (path) => {
+    navigate(path);
+  };
 
   const onTitleChange = (e) => setTitle(e.target.value);
   const onDescriptionChange = (e) => setDescription(e.target.value);
@@ -17,7 +24,6 @@ const Courseform = () => {
   const onInstructorChange = (e) => setInstructor(e.target.value);
 
   const handleSubmit = (e) => {
-    e.preventDefault();
     const formData = new FormData();
     formData.append('title', title);
     formData.append('description', description);
@@ -25,6 +31,12 @@ const Courseform = () => {
     formData.append('instructor_name', instructor);
     formData.append('upload_preset', 'penz-classes');
     dispatch(addNewCourses(formData));
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.stopPropagation();
+      e.preventDefault();
+    }
+    setValidated(true);
     setTitle('');
     setDescription('');
     setImage('');
@@ -33,17 +45,17 @@ const Courseform = () => {
 
   return (
     <div>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit} noValidate validated={validated}>
         <h2>Add course</h2>
         <Form.Label>Add course title</Form.Label>
-        <Form.Control onChange={onTitleChange} type="text" />
+        <Form.Control onChange={onTitleChange} type="text" required />
         <Form.Label>Add course description</Form.Label>
-        <Form.Control onChange={onDescriptionChange} type="text" />
+        <Form.Control onChange={onDescriptionChange} type="text" required />
         <Form.Label>Add image</Form.Label>
-        <Form.Control onChange={onImageChange} type="file" accept="image/*" />
+        <Form.Control onChange={onImageChange} type="file" accept="image/*" required />
         <Form.Label>Add instructor name</Form.Label>
-        <Form.Control onChange={onInstructorChange} type="text" />
-        <Button type="submit">Add course</Button>
+        <Form.Control onChange={onInstructorChange} type="text" required />
+        <Button type="submit" onClick={() => handleRoutes('/courses')}>Add course</Button>
       </Form>
     </div>
   );
