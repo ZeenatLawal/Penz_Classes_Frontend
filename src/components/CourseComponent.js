bafc1806a703ebacf9d56e5925b518357fa72643
elyor doniyorov <doniyorovelyor@gmail.com>
2021-11-23 13:14:47 +0500

    Delete item.js

--- src/components/CourseComponent.js
+++ src/components/CourseComponent.js
@@ -1,26 +1,38 @@
-/* eslint-disable */
-import React from "react";
-import { useDispatch } from "react-redux";
+import React from 'react';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-export const CourseComponent = ({ course }) => {
-  const dispatch = useDispatch();
-  return (
-      <div key={course.id}>
-        <div className="d-flex pt-5">
-          <div className="d-flex flex-column p-2">
-            <img src={course.image} alt="First slide" />
-            <hr className="w-50 mx-auto" />
-            <h5 className="text-center">{course.title}</h5>
-            <p className="w-100 text-break description">{course.description}</p>
-            <div className="d-flex p-3 justify-content-evenly">
-              <FaFacebook />
-              <FaTwitter />
-              <FaInstagram />
-            </div>
-          </div>
+const CourseComponent = ({ course }) => (
+  <div key={course.id}>
+    <div className="d-flex pt-5">
+      <div className="d-flex flex-column p-2">
+        <Link to={`/courses/${course.id}`}>
+          {' '}
+          <img src={course.image} alt="course slides" />
+          {' '}
+        </Link>
+        <hr className="w-50 mx-auto" />
+        <h5 className="text-center">{course.title}</h5>
+        <p className="w-100 text-break description">{course.description}</p>
+        <div className="d-flex p-3 justify-content-evenly">
+          <FaFacebook />
+          <FaTwitter />
+          <FaInstagram />
+        </div>
       </div>
-   </div>
-  )
+    </div>
+  </div>
+);
+
+CourseComponent.propTypes = {
+  course: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    instructor: PropTypes.string,
+    image: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
 };
 
+export default CourseComponent;
--- src/components/CourseListing.js
+++ src/components/CourseListing.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { loadCourses } from "../redux/courses/Courses";
-import { StyledComponent, CourseComponent } from "./CourseComponent";
-import Carousel from "react-elastic-carousel";
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Carousel from 'react-elastic-carousel';
+import { loadCourses } from '../redux/courses/Courses';
+import CourseComponent from './CourseComponent';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 558, itemsToShow: 2 },
   { width: 768, itemsToShow: 3 },
   { width: 1200, itemsToShow: 4 },
-]
+];
 
 const CourseListing = () => {
   const courses = useSelector((state) => state.coursesReducer.courses);
@@ -19,15 +18,15 @@ const CourseListing = () => {
     dispatch(loadCourses());
   }, [dispatch]);
   return (
-    <>  
-        <Carousel breakPoints={breakPoints}>
-          {courses && courses.map((course) => (
-                <CourseComponent
-                  key={course.id}
-                  course={course}
-                />
-            ))}
-        </Carousel>
+    <>
+      <Carousel breakPoints={breakPoints}>
+        {courses && courses.map((course) => (
+          <CourseComponent
+            key={course.id}
+            course={course}
+          />
+        ))}
+      </Carousel>
     </>
   );
 };
